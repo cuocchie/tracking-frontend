338f0f8561776078b5b26bce04df0b11f09922a1
datnt43 <datnt43@techcombank.com.vn>
2025-05-08 01:04:59 +0700

    Updates

--- src/data/practiceData.js
+++
@@ -1,3 +0,0 @@
-// Global data for practiced and planned practice dates
-export const practicedDates = ["2025-05-02", "2025-05-03", "2025-05-04", "2025-05-06"];
-export const plannedPracticeDates = ["2025-05-10", "2025-05-15", "2025-05-20"];
